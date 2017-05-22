85a49901057689d05cbefd447790f77b233eb44f
Jarkko Mönkkönen <jarkko.monkkonen@happy-or-not.com>
2017-05-22 10:35:32 +0300

    Helper code to remove deviceownership

--- cosu_codelab_complete/app/src/main/java/com/google/codelabs/cosu/MainActivity.java
+++ cosu_codelab_complete/app/src/main/java/com/google/codelabs/cosu/MainActivity.java
@@ -1,11 +1,11 @@
 // Copyright 2016 Google Inc.
-// 
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -95,8 +95,14 @@ public class MainActivity extends Activity {
         mDevicePolicyManager = (DevicePolicyManager)
                 getSystemService(Context.DEVICE_POLICY_SERVICE);
 
+        /* UNCOMMENT TO REMOVE DEVICE OWNER!
+        Log.i("COSU", "try to remove device owner...");
         mAdminComponentName = DeviceAdminReceiver.getComponentName(this);
-
+        mDevicePolicyManager = (DevicePolicyManager) getSystemService(Context.DEVICE_POLICY_SERVICE);
+        mDevicePolicyManager.removeActiveAdmin(mAdminComponentName);
+        mDevicePolicyManager.clearDeviceOwnerApp(getPackageName());
+        Log.i("COSU", "removed device owner?");
+        */
         mPackageManager = this.getPackageManager();
 
         lockTaskButton = (Button) findViewById(R.id.start_lock_button);
